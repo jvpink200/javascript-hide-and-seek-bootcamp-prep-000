fabad97b339388fb9ef8f2c4864665af852fdc4a
jvpink200 <jvillarr20@gmail.com>
2018-07-24 00:18:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var ranks = document.getSelectorAll('ul.ranked-list li');
+  var ranks = document.querySelectorAll('ul.ranked-list li');
   for(var i = 0; i<ranks.length; i++){
     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
   }
